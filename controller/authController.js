5fbd5842ed9ccb1a1409b5b02610d62c577ecde1
Zihadul Islam <zihadul10101@gmail.com>
2022-04-25 11:02:19 +0600

    use bcrypt validator

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,44 @@
-module.exports.admin_login =(req, res) => {
-    console.log(req.body);
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+const adminModel = require('../models/adminModel')
+module.exports.admin_login = async (req, res) => {
+    // console.log(req.body);
+    const { email, password } = req.body;
+
+    const error = {
+
+    }
+
+    if (email && !validator.isEmail(email)) {
+        error.email = 'please provide your valid email'
+    }
+    if (!email) {
+        error.email = 'please provide your  email'
+    }
+    if (!password) {
+        error.password = 'please provide your password'
+    }
+    if (Object.keys(error).length > 0) {
+        return res.status(400).json({ errorMessage: error })
+    } else {
+        try {
+            const getAdmin = await adminModel.findOne({ email }).select('+password');
+            // console.log(getAdmin);
+            if (getAdmin) {
+                const matchPassword = await bcrypt.compare(password, getAdmin.password);
+                // console.log(matchPassword);
+                if (matchPassword) {
+
+                }
+                else {
+                    return res.status(404).json({ errorMessage: 'Password does not exist' });
+
+                }
+            } else {
+                return res.status(404).json({ errorMessage: 'Email does not exist' });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }

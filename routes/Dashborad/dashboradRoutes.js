2328a6d31f7819b6802290c9496b8a75b6e97565
Zihadul Islam <zihadul10101@gmail.com>
2022-05-20 15:52:36 +0600

    get delete id in backend

--- routes/Dashborad/dashboradRoutes.js
+++ routes/Dashborad/dashboradRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const {category_add,category_get} = require('../../controller/Dashborad/categoryController')
+const {category_add,category_get,category_delete} = require('../../controller/Dashborad/categoryController')
 const {admin_middleware} = require('../../middlewar/authMiddlewar')
 
 router.post('/add-category',admin_middleware,category_add)
 router.get('/get-category',admin_middleware,category_get)
+router.delete('/delete-category/:categoryId',admin_middleware,category_delete)
 
 module.exports = router;

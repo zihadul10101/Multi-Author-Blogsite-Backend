c730c313b17c62d41a96596591f233386cadb485
Zihadul Islam <zihadul10101@gmail.com>
2022-06-01 12:39:08 +0600

    Article add successfull but error not get

--- controller/Dashborad/articalController.js
+++ controller/Dashborad/articalController.js
@@ -3,6 +3,7 @@ const tagModel = require('../../models/tagModel');
 const formidable = require('formidable');
 const { article_validator } = require('../../validator/validator')
 const fs = require('fs');
+const articleModel = require('../../models/articleModel');
 
 module.exports.get_tag_category = async (req, res) => {
 
@@ -19,32 +20,61 @@ module.exports.get_tag_category = async (req, res) => {
     }
 }
 module.exports.add_artical = (req, res) => {
+
     const formDataHendle = formidable({
         multiples: true
-    });
+    })
     const { adminId, adminName } = req;
-    // console.log(adminName)
-    formDataHendle.parse(req, (err, fields, files) => {
-        if (!err) {
-            const { title, category, slug, tag, text } = fields;
 
-            const validate = article_validator(fields, files)
-            if (!validate.validated) {
+    formDataHendle.parse(req, (err, fields, files) => {
+        
+        if (!err) {
+
+            const { title, category, slug, tag, text } = fields;
+            const validate = article_validator(fields, files);
+            if (validate.validated) {
                 const categoryName = category.split('-').join(' ');
                 const tagName = tag.split('-').join(' ');
                 files.image.originalFilename = Date.now() + files.image.originalFilename;
-                // console.log(files.image.originalFilename)
+
                 const uploadPath = __dirname + `../../../../frontend/public/articalImage/${files.image.originalFilename}`;
-                // console.log(uploadPath)
-                fs.copyFile(files.image.filepath, uploadPath, error => {
+
+                fs.copyFile(files.image.filepath, uploadPath, async (error) => {
                     if (error) {
-                        console.log(error);
+                        res.status(400).json({
+                            errorMessage: {
+                                imageUpload: 'Image upload file'
+                            }
+                        })
                     } else {
-                        console.log('file updated successfully')
+                        try {
+                            await articleModel.create({
+                                adminId,
+                                adminName,
+                                title,
+                                slug,
+                                category: categoryName,
+                                category_slug: category,
+                                tag: tagName,
+                                tag_slug: tag,
+                                articleText: text,
+                                image: files.image.originalFilename
+                            })
+                            res.status(201).json({
+                                successMessage: 'Article add successfull'
+                            })
+                        } catch (error) {
+                            console.log(error.message)
+                            res.status(500).json({
+                                errorMessage: {
+                                    error: 'Internal server error'
+                                }
+                            })
+                        }
                     }
                 })
             } else {
-                res.status(400).json({ errorMessage: validate.error })
+                res.status(400).json({ errorMessage: validate.error })  
             }
         }
     })

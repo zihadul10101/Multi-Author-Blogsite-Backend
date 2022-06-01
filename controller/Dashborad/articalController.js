9446496781b72ce9bfbdc2edfc08075e836b6ed3
Zihadul Islam <zihadul10101@gmail.com>
2022-06-01 11:13:32 +0600

    file upload successfully

--- controller/Dashborad/articalController.js
+++ controller/Dashborad/articalController.js
@@ -2,6 +2,7 @@ const categoryModel = require('../../models/categoryModel');
 const tagModel = require('../../models/tagModel');
 const formidable = require('formidable');
 const { article_validator } = require('../../validator/validator')
+const fs = require('fs');
 
 module.exports.get_tag_category = async (req, res) => {
 
@@ -21,11 +22,30 @@ module.exports.add_artical = (req, res) => {
     const formDataHendle = formidable({
         multiples: true
     });
+    const { adminId, adminName } = req;
+    // console.log(adminName)
     formDataHendle.parse(req, (err, fields, files) => {
         if (!err) {
-            const { title, category, slug, tag, text } = fields;  
-            const validate= article_validator( fields, files)
-            console.log(validate)
+            const { title, category, slug, tag, text } = fields;
+
+            const validate = article_validator(fields, files)
+            if (!validate.validated) {
+                const categoryName = category.split('-').join(' ');
+                const tagName = tag.split('-').join(' ');
+                files.image.originalFilename = Date.now() + files.image.originalFilename;
+                // console.log(files.image.originalFilename)
+                const uploadPath = __dirname + `../../../../frontend/public/articalImage/${files.image.originalFilename}`;
+                // console.log(uploadPath)
+                fs.copyFile(files.image.filepath, uploadPath, error => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('file updated successfully')
+                    }
+                })
+            } else {
+                res.status(400).json({ errorMessage: validate.error })
+            }
         }
     })
 }
--- middlewar/authMiddlewar.js
+++ middlewar/authMiddlewar.js
@@ -1,8 +1,14 @@
-module.exports.admin_middleware = (req, res, next) => {
-   const {blog_token}=req.cookies;
- if(!blog_token){
-    return res.status(404).json({ errorMessage: { error: "Please login first" } });
- }else{
-     next();
- }
+const jwt = require('jsonwebtoken');
+module.exports.admin_middleware = async(req, res, next) => {
+   const { blog_token } = req.cookies;
+   if (!blog_token) {
+      return res.status(404).json({ errorMessage: { error: "Please login first" } });
+   } else {
+      const deCodeToken = await jwt.verify(blog_token,'zihadulid');
+      // console.log(deCodeToken)
+      req.adminId = deCodeToken.id;
+      req.adminName  = deCodeToken.name;
+      req.role = deCodeToken.role
+      next();
+   }
 }
